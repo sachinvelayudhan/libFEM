066560ba2ff1d077eb4b9a3e88ef228ed1e6acc6
bensingh <bensingh@iisc.ac.in>
2022-05-24 16:49:21 -0500

    added the quadratic basis functions

--- C_FEM_BasisFunction_2D_TRIA.h
+++ C_FEM_BasisFunction_2D_TRIA.h
@@ -48,6 +48,44 @@ class C_TriangleBasis
             }
             (*this).num_sp=num_sp;
         }
+        else
+        {
+           // Quadratic
+            num_sp = 6; 
+
+            // Assign new, correctly-sized dense matrices to member variables
+            (*this).sp = new C_Matrix_Dense [GP_Data.num_GP];
+            (*this).dsp = new C_Matrix_Dense [GP_Data.num_GP];
+
+            for (int ii = 0; ii < GP_Data.num_GP; ii++)
+            {
+                double s = GP_Data.ptx[ii];
+                double t = GP_Data.pty[ii];
+                (this -> sp)[ii].reshape(1,num_sp);
+                (this -> dsp)[ii].reshape(2,num_sp);
+
+                (this -> sp)[ii](0,0) = s*(2*s - 1);
+                (this -> sp)[ii](0,1) = 4*s*t;
+                (this -> sp)[ii](0,2) = 4*s*(1 - s - t);
+                (this -> sp)[ii](0,3) = t*(2*t - 1);
+                (this -> sp)[ii](0,4) = 4*t*(1 - s - t);
+                (this -> sp)[ii](0,5) = (1 - s - t)*(2*(1 - s - t) - 1);
+
+                (this -> dsp)[ii](0,0) = 4*s - 1;
+                (this -> dsp)[ii](0,1) = 4*t;
+                (this -> dsp)[ii](0,2) = 4 - 8*s - 4*t;
+                (this -> dsp)[ii](0,3) = 0;
+                (this -> dsp)[ii](0,4) = -4*t;
+                (this -> dsp)[ii](0,5) = 4*t + 4*s - 3;
+
+                (this -> dsp)[ii](1,0) = 0;
+                (this -> dsp)[ii](1,1) = 4*s;
+                (this -> dsp)[ii](1,2) = -4*s;
+                (this -> dsp)[ii](1,3) = 4*t - 1;
+                (this -> dsp)[ii](1,4) = 4 - 8*t - 4*s;
+                (this -> dsp)[ii](1,5) = 4*t + 4*s - 3; 
+            } 
+        }
     }
     ~C_TriangleBasis()
     {
