c2f0e3773a87cfba5fbb7a09f3cbf0c1d12cbc03
sachinvelayudhan <sachin@tamu.edu>
2022-06-05 12:16:32 -0500

    StiffnessQuad

---
+++ inc/f_StiffnessLaplace.h
@@ -0,0 +1,15 @@
+#include<vector>
+
+void stiffnessLaplace(const std::vector<std::vector<double>>& elNodes, const std::vector<int>& elCon, C_Matrix_Sparse& kGlob, C_QuadrilateralBasis& feL, const C_GaussData2DQuad& gpData)
+{
+    C_Matrix_Dense ke(4,4);
+    double jac;
+    double JxW;
+    for(int itGp=0; itGp<gpData.num_GP;itGp++)
+    {
+        C_Matrix_Dense& dsp=feL.getDerShapeFunction(itGp, elNodes,jac);
+        JxW=jac*gpData.wt[itGp];
+        ke+=JxW*(dsp.T()*dsp);
+    }
+    std::cout<< ke;
+}
