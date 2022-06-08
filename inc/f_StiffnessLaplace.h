975f801f5cadac9ea95cc5311ed3ec22c8a98c5a
sachinvelayudhan <sachin@tamu.edu>
2022-06-08 13:18:55 -0500

    Code Updated

--- inc/f_StiffnessLaplace.h
+++ inc/f_StiffnessLaplace.h
@@ -1,17 +1,26 @@
 #include<vector>
 
-void stiffnessLaplace(const std::vector<std::vector<double>>& elNodes, const std::vector<int>& elCon, C_Matrix_Sparse& kGlob, C_QuadrilateralBasis& feL, const C_GaussData2DQuad& gpData, C_Matrix_Dense& A)
+void stiffnessLaplace(const std::vector<std::vector<double>>& elNodes, const std::vector<int>& elCon, C_Matrix_Sparse& kGlob, C_Matrix_Sparse& fGlob, C_QuadrilateralBasis& feL, const C_GaussData2DQuad& gpData, C_Matrix_Dense& A)
 {
-    C_Matrix_Dense ke(4,4);
+    C_Matrix_Dense ke(4,4), fe(4,1);
+    const std::vector<int> one_vec = {0}; // Vector of value 1
     double jac;
     double JxW;
+
+    //one_vec = 
+
     for(int itGp = 0; itGp < gpData.num_GP; itGp++)
     {
         C_Matrix_Dense& dsp = feL.getDerShapeFunction(itGp, elNodes, jac);
+        C_Matrix_Dense& spR = feL.getShapeFunction(itGp);
         JxW = jac*gpData.wt[itGp];
         ke += JxW*(dsp.T()*A*dsp);
+        fe += JxW*spR.T();
     }
-    std::cout<< ke;
+
+    // std::cout<< ke;
+    // std::cout<< fe;
 
     kGlob.add_matr(ke, elCon, elCon); 
+    fGlob.add_matr(fe, elCon, one_vec); 
 }
--- mainLaplaceQuad1.cpp
+++ mainLaplaceQuad1.cpp
@@ -16,8 +16,9 @@ int main()
     C_Mesh2D mesh;
 
     int noX = 2, noY = 2, IEL = 1, NNM, NDF = 1, totDof;
-    mesh.meshRectangle({0,1},{0,1},noX,noY);
-    C_Matrix_Sparse kGlob(mesh.num_Nd,mesh.num_Nd);
+    double x0 = 0, xa = 1.0, y0 = 0, yb = 1.0;
+    mesh.meshRectangle({x0,xa},{y0,yb},noX,noY);
+    C_Matrix_Sparse kGlob(mesh.num_Nd,mesh.num_Nd), fGlob(mesh.num_Nd,1);
     C_QuadrilateralBasis feL(1, gpData);
     int itEl = 0;
 
@@ -32,20 +33,13 @@ int main()
  
     for (const auto& elCon: mesh.elements)
     {
-        std::cout << (itEl+1);
+        // std::cout << (itEl+1);
         std::vector<std::vector<double>> elNodes = mesh.getElNodes(itEl);
-        stiffnessLaplace(elNodes, elCon, kGlob, feL, gpData, A);
+        stiffnessLaplace(elNodes, elCon, kGlob, fGlob, feL, gpData, A);
         itEl++;
     }  
 
     Eigen::VectorXd fGlobal(totDof);
-    // fGlobal = fGlobal + Eigen::VectorXd::Constant(totDof,1);
-
-    // i. Neumann Boundary Conditions
-    // int udl = 1;
-    // fGlobal = fGlobal*udl;
-
-    std::cout << fGlobal;
 
      // ii. Dirichlet Boundary Conditions
     int bc, NSPV = 5;
@@ -56,13 +50,23 @@ int main()
     ISPV = {2,5,6,7,8};
     VSPV = {0,0,0,0,0};
 
+    // 4 x 4 Mesh
+    // ISPV = {4,9,14,19,20,21,22,23,24};
+    // VSPV = {0,0,0,0,0,0,0,0,0};
+
     for(int ii = 0; ii < NSPV; ii++)
     {
         bc = ISPV[ii];
         kGlob.col_NonSparseAssign(0.0, bc);
         kGlob.row_NonSparseAssign(0.0, bc);
         kGlob(bc,bc) = 1.0;
-        fGlobal(bc) = VSPV[ii]; 
+        fGlob(bc,0) = VSPV[ii]; 
+    }
+
+    // Converting force vector to eigen::force vector
+    for(int i = 0; i < totDof; i++)
+    {
+        fGlobal(i) = fGlob(i,0);
     }
 
     // Solve, using Cholesky Factorization of kGlob
